1a42fa407ae57294645f9a7810c0461cc10ab2cd
jjavier-chool <178938739+jjavier-chool@users.noreply.github.com>
2026-06-15 16:32:46 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         match command:
+            case exit:
+                break;
             case _:
                 print(f"{command}: command not found")
     pass

d3b9c4d894959b2af3357f4f0ec99760bcc31261
jjavier-chool <178938739+jjavier-chool@users.noreply.github.com>
2026-06-16 17:22:46 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,18 +1,60 @@
 import subprocess
-import shlex # This library is basically like cheating for all of the quoting challenges. Want to try writing it myself later
+import readline
+import shlex # This library is basically like cheating for the quoting challenges. Want to try writing it myself later
 import sys
 import os
 
+# Currently defined built-in commands
 builtin = ["pwd", "type", "echo", "exit"]
 
-def isExecutable(command):
+def completer(text: str, state: int) -> str | None:
+    """Tab autocompletion
+
+    This function finds a matching built-in command according to the given prefix and state.
+
+    Args:
+        text (str): The given user text.
+        state (int): The index of the current matching command, incremented by readline.
+
+    Returns:
+        str: The name of the matching built-in command.
+        None: No matching built-in command found.
+    """
+    matches = [command for command in builtin if command.startswith(text)]
+
+    if state < len(matches):
+        return matches[state] + " "
+
+    return None
+
+def isExecutable(command: str) -> tuple[bool, str]:
+    """Determines whether a given command is executable
+
+    This function searches for an executable file with the given name.
+
+    Args:
+        command (str): The given command to investigate.
+
+    Returns:
+        tuple[bool, str]: T/F whether the command is executable, the command's path
+    """
     path = os.environ["PATH"]
     for dir in path.split(os.pathsep):
         if os.access(dir + "/" + command, os.X_OK):
             return True, dir + "/" + command
     return False, ""
 
-def type(command):
+def type(command: str) -> tuple[str, bool]:
+    """Type built-in
+
+    This function determines the type of the given command: builtin, executable, or not found.
+
+    Args:
+        command (str): The command requested by the user
+
+    Returns:
+        tuple[str, bool]: The proper type reporting, T/F whether the command was found
+    """
     executable, dir = isExecutable(command)
     if command in builtin:
         return f"{command} is a shell builtin" + "\n", False
@@ -21,13 +63,27 @@ def type(command):
     else:
         return f"{command}: not found" + "\n", True
 
-def main():
+def main() -> None:
+    """Main parsing logic of Shell
+
+    This function performs the majority of the required logic. Parses user input,
+    performs built-in processes when necessary, or executes a given process if found.
+    Redirects to files when specified by user input.
+
+    """
+    readline.set_completer(completer)
+    readline.parse_and_bind("tab: complete")
+
     while True:
+        # Print $ and obtain user input.
         sys.stdout.write("$ ")
         command = input()
+        if not command:
+            continue
         command_split = shlex.split(command)
         process = command_split[0]
 
+        # Checking if there are requested stdout and stderr redirects to files.
         redirect = False
         redirectErr = False
         append = False
@@ -53,9 +109,16 @@ def main():
                 outputFile = command_split[-1]
                 command_split = command_split[:-2]
 
+        # Performing the user's given process.
         match process:
             case "exit":
                 exit(0)
+            case "echo":
+                output = " ".join(command_split[1:]) + "\n"
+            case "type":
+                output, error = type(command[5:])
+            case "pwd":
+                output = os.getcwd() + "\n"
             case "cd":
                 abspath = command[3:]
                 if abspath == "~":
@@ -64,13 +127,8 @@ def main():
                     os.chdir(abspath)
                 else:
                     error = True
-                    output = f"cd: {abspath}: No such file or directory" + "\n" # Naive error, no msg for not a directory specifically
-            case "echo":
-                output = " ".join(command_split[1:]) + "\n"
-            case "type":
-                output, error = type(command[5:])
-            case "pwd":
-                output = os.getcwd() + "\n"
+                    # Naive error: no specific message given for non-directory
+                    output = f"cd: {abspath}: No such file or directory" + "\n"
             case _:
                 executable, _ = isExecutable(process)
                 if executable:
@@ -92,6 +150,8 @@ def main():
                 else:
                     error = True
                     output = f"{command}: command not found" + "\n"
+
+        # Perform redirecting/appending, or write to stdout.
         if redirect or redirectErr:
             with open(outputFile, "w") as file:
                 if redirectErr and not error:

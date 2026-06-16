608a681298d1e0d16152470e205b1515389ae61c
jjavier-chool <178938739+jjavier-chool@users.noreply.github.com>
2026-06-16 15:14:28 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def main():
         redirect = false
         output = ""
         outputFile = ""
-        if command_split[-2] == ">" || command_split[-2] == "1>":
+        if command_split[-2] == ">" | command_split[-2] == "1>":
             redirect = true
             outputFile = command_split[-1]
 
